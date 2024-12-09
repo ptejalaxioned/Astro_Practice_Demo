94d99f6c9f7813e1280b5e87048e85626137b2d4
ptejalaxioned <ptejalaxioned@gmail.com>
2024-12-09 17:41:54 +0530

    git hub workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
+  site: 'https://github.com/Astro_Project_Demo.git',
+  base: 'https://github.com/ptejalaxioned/Astro_Project_Demo.git',
   integrations: [react(), vue()],
   vite: {
     resolve: {
